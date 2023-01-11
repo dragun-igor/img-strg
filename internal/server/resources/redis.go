f5a626ae3805ce86b079d041a4ee92bfb5936d13
Dragun Igor <dragun.msc@gmail.com>
2023-01-11 03:35:26 +0300

    add timeout const

--- internal/server/resources/redis.go
+++ internal/server/resources/redis.go
@@ -8,14 +8,19 @@ import (
 	"github.com/dragun-igor/img-strg/pkg/redis"
 )
 
+const (
+	connectionTimeout = time.Second * 5
+	operationTimeout  = time.Second * 2
+)
+
 // Инициализация редиса
 func InitRedis(config *config.Config) (*redis.Client, error) {
 	client, err := redis.NewClient(redis.Config{
 		Host:              config.RedisHost,
 		Port:              config.RedisPort,
 		Password:          config.RedisPassword,
-		ConnectionTimeout: time.Second * 5,
-		OperationTimeout:  time.Second * 2,
+		ConnectionTimeout: connectionTimeout,
+		OperationTimeout:  operationTimeout,
 	})
 	if err != nil {
 		return nil, err
